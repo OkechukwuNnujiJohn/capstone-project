1956e9e2873f269689e22c174fd8c78e6207f668
Okechukwu Nnuji-John <nnujijohn@meta.com>
2023-07-17 21:56:31 -0700

    All-these-are-in-built

--- J.Thrift/J.Thrift-api/models/index.js
+++ J.Thrift/J.Thrift-api/models/index.js
@@ -10,5 +10,4 @@ Cart.belongsTo(User, { as: 'user', foreignKey: 'userId' });
 Cart.belongsToMany(Item, { through: CartItem});
 Item.belongsToMany(Cart, { through: CartItem});
 
-
 export { User,Item, Cart, CartItem };
